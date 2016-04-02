feea70069740b419278e83a772fbf94f69b850bd
ellisp <peter.ellis2013nz@gmail.com>
2016-04-03 09:00:51 +1200

    additional unit tests

--- pkg/tests/testthat/test_totals_v_published.R
+++ pkg/tests/testthat/test_totals_v_published.R
@@ -3,6 +3,8 @@ require(dplyr)
 
 # Must match results from 
 # http://www.electionresults.govt.nz/electionresults_2014/e9/html/e9_part1.html
+
+# absolute numbers
 expect_that(
     sum(filter(GE2014, Party == "National Party" & VotingType == "Party")$Votes),
     equals(1131501)
@@ -37,4 +39,19 @@ expect_that(
 expect_that(
     sum(filter(GE2014, Party == "Patriotic Revolutionary Front" & VotingType == "Candidate")$Votes),
     equals(48)
+)
+
+
+# percentages
+expect_that(
+    round(sum(filter(GE2014, Party == "National Party" & VotingType == "Party")$Votes) /
+        sum(filter(GE2014, VotingType == "Party" & Party != "Informal Party Votes")$Votes) * 100, 2),
+    equals(47.04)
+ )
+
+
+expect_that(
+    round(sum(filter(GE2014, Party == "Green Party" & VotingType == "Candidate")$Votes) /
+              sum(filter(GE2014, VotingType == "Candidate" & Party != "Informal Candidate Votes")$Votes) * 100, 2),
+    equals(7.06)
 )
