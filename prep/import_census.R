b75accb75c16a23fed23f4c1dbd4aa3438c19af6
ellisp <peter.ellis2013nz@gmail.com>
2016-04-12 08:18:33 +1200

    Add selected individual part 1 meshblock data

--- prep/import_census.R
+++ prep/import_census.R
@@ -1,14 +1,14 @@
 
-download.file("http://www3.stats.govt.nz/meshblock/2013/csv/2013_mb_dataset_Total_New_Zealand_CSV.zip",
-              destfile = "downloads/census2013/2013_mb_census.zip", mode = "wb")
-
-unzip("downloads/census2013/2013_mb_census.zip", exdir = "downloads/census2013")
+# download.file("http://www3.stats.govt.nz/meshblock/2013/csv/2013_mb_dataset_Total_New_Zealand_CSV.zip",
+#               destfile = "downloads/census2013/2013_mb_census.zip", mode = "wb")
+# 
+# unzip("downloads/census2013/2013_mb_census.zip", exdir = "downloads/census2013")
 
 
 
 #-------------------selected dwellings variables-----------------
 tmp_orig <- read.csv("downloads/census2013/2013-mb-dataset-Total-New-Zealand-Dwelling.csv",
-                     stringsAsFactors = FALSE, na.strings = "..C")
+                     stringsAsFactors = FALSE, na.strings = c("..C", "*"))
 
 
 dwelling <- tmp_orig %>%
@@ -24,7 +24,7 @@ dwelling <- tmp_orig %>%
 
 #-------------------selected household variables--------------------
 tmp_orig <- read.csv("downloads/census2013/2013-mb-dataset-Total-New-Zealand-Household.csv",
-                     stringsAsFactors = FALSE, na.strings = "..C")
+                     stringsAsFactors = FALSE, na.strings = c("..C", "*"))
 
 hh <- tmp_orig %>%
     rename(MB = Code) %>%
@@ -50,13 +50,47 @@ hh <- tmp_orig %>%
         PropNoMotorVehicle2013 = X2013_Census_number_of_motor_vehicles_for_households_in_occupied_private_dwellings_No_Motor_Vehicle / 
             X2013_Census_number_of_motor_vehicles_for_households_in_occupied_private_dwellings_Total_households_in_occupied_private_dwellings   
         ) %>%
-    select(MB, NumberInHH2013, PropMultiPersonHH2013, PropInterneHH2013,
+    select(MB, NumberInHH2013, PropMultiPersonHH2013, PropInternetHH2013,
            PropNotOwnedHH2013, MedianRentHH2013, PropLandlordPublic2013,
-           NoMotorVehicle2013)
+           PropNoMotorVehicle2013)
 
+
+#------------selected individuals 1 variables-----------
+indiv1_orig <- read.csv("downloads/census2013/2013-mb-dataset-Total-New-Zealand-Individual-Part-1.csv",
+                        stringsAsFactors = FALSE, na.strings = c("..C", "*"))
+names(indiv1_orig)[grep("X2013", names(indiv1_orig))]
+
+indiv1 <- indiv1_orig %>%
+    rename(MB = Code) %>%
+    mutate(
+        PropOld2013 = X2013_Census_age_in_broad_groups_for_the_census_usually_resident_population_count.1._65_years_and_Over / 
+            X2013_Census_age_in_broad_groups_for_the_census_usually_resident_population_count.1._Total_people,
+        PropEarly20s2013 = X2013_Census_age_in_five.year_groups_for_the_census_usually_resident_population_count.1._20.24_Years / 
+            X2013_Census_age_in_five.year_groups_for_the_census_usually_resident_population_count.1._Total_people,
+        PropChildren2013 = X2013_Census_age_in_broad_groups_for_the_census_usually_resident_population_count.1._Under_15_years /
+            X2013_Census_age_in_broad_groups_for_the_census_usually_resident_population_count.1._Total_people,
+        PropSameResidence2013 = X2013_Census_usual_residence_five_years_ago_.2008._indicator_for_the_census_usually_resident_population_count.1._Same_as_Usual_Residence /
+            X2013_Census_usual_residence_five_years_ago_.2008._indicator_for_the_census_usually_resident_population_count.1._Total_people,
+        PropOverseas2013 = X2013_Census_usual_residence_five_years_ago_.2008._indicator_for_the_census_usually_resident_population_count.1._Overseas /
+            X2013_Census_usual_residence_five_years_ago_.2008._indicator_for_the_census_usually_resident_population_count.1._Total_people,
+        PropNZBorn2013 = X2013_Census_birthplace_for_the_census_usually_resident_population_count.1._NZ_born /
+            X2013_Census_birthplace_for_the_census_usually_resident_population_count.1._Total_people,
+        PropEuropean2013 = X2013_Census_ethnic_group_.grouped_total_responses..7..8._for_the_census_usually_resident_population_count.1._European /
+            X2013_Census_ethnic_group_.grouped_total_responses..7..8._for_the_census_usually_resident_population_count.1._Total_people,
+        PropMaori2013 = X2013_Census_ethnic_group_.grouped_total_responses..7..8._for_the_census_usually_resident_population_count.1._Maori /
+            X2013_Census_ethnic_group_.grouped_total_responses..7..8._for_the_census_usually_resident_population_count.1._Total_people,
+        PropPacific2013 = X2013_Census_ethnic_group_.grouped_total_responses..7..8._for_the_census_usually_resident_population_count.1._Pacific_Peoples /
+            X2013_Census_ethnic_group_.grouped_total_responses..7..8._for_the_census_usually_resident_population_count.1._Total_people,
+        PropAsian2013 = X2013_Census_ethnic_group_.grouped_total_responses..7..8._for_the_census_usually_resident_population_count.1._Asian /
+            X2013_Census_ethnic_group_.grouped_total_responses..7..8._for_the_census_usually_resident_population_count.1._Total_people
+     ) %>%
+    select(MB, PropOld2013, PropEarly20s2013, PropChildren2013, PropSameResidence2013,
+           PropOverseas2013, PropNZBorn2013, PropEuropean2013, PropMaori2013, 
+           PropPacific2013, PropAsian2013)
 
 #-----------------------finish-------------------
 Meshblocks2013 <- dwelling %>%
-    left_join(hh, by = "MB")
+    left_join(hh, by = "MB") %>%
+    left_join(indiv1, by = "MB")
 save(Meshblocks2013, file = "pkg/data/Meshblocks2013.rda")
      
