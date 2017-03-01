3ed0531a8d26d27053de61951b57b56359eb54c2
Peter Ellis <peter.ellis2013nz@gmail.com>
2017-03-01 16:36:50 +1300

    Add polls for 2008 election

--- pkg1/R/parties_doc.R
+++ pkg1/R/parties_doc.R
@@ -26,6 +26,9 @@
 #' \code{EndData} and \code{StartDate} refer to the data collection period.  These
 #' dates were entered by hand, use with caution and check against \code{WikipediaDates} 
 #' which is the version from the original source.
+#' 
+#' Where the date in Wikipedia is given only as "released on X", the start and end dates
+#' have been deemed to be two days before release.
 #'
 #' @format A data frame of 7 columns
 #' @source \url{https://en.wikipedia.org/wiki/Opinion_polling_for_the_New_Zealand_general_election,_2014}
--- prep/combine_polls.R
+++ prep/combine_polls.R
@@ -1,5 +1,5 @@
 
-polls <- plyr::rbind.fill(polls2011, polls2014, polls2017) %>%
+polls <- plyr::rbind.fill(polls2008, polls2011, polls2014, polls2017) %>%
     mutate(MidDate = StartDate + (EndDate - StartDate) / 2) %>%
     gather(Party, VotingIntention, -StartDate, -EndDate, -MidDate, -Poll, -WikipediaDates) %>%
     mutate(Poll = str_trim(gsub("\\[.+\\]", "", Poll)),
@@ -8,10 +8,26 @@ polls <- plyr::rbind.fill(polls2011, polls2014, polls2017) %>%
     # clean up pollster names:
     mutate(Poll = gsub(".*[Ee]lection result", "Election result", Poll),
            Poll = gsub("Herald.Digi[Pp]oll", "Herald-Digipoll", Poll)) %>%
-    arrange(MidDate)
+    arrange(MidDate) %>%
+    mutate(Client = ifelse(grepl("Fairfax", Poll), "Fairfax Media", NA),
+           Client = ifelse(grepl("Herald", Poll), "Herald", Client),
+           Client = ifelse(grepl("3 New", Poll), "3 News", Client),
+           Client = ifelse(grepl("One News", Poll), "One News", Client),
+           Client = ifelse(grepl("Sunday Star", Poll), "Sunday Star Times", Client),
+           Client = ifelse(grepl("Newshub", Poll), "Newshub", Client)) %>%
+    mutate(Poll = gsub("Fairfax Media.", "", Poll),
+           Poll = gsub("One News ", "", Poll),
+           Poll = gsub("3 News ", "", Poll),
+           Poll = gsub("Newshub ", "", Poll),
+           Poll = gsub("Herald.", "", Poll),
+           Poll = ifelse(grepl("Roy Morgan", Poll), "Roy Morgan", Poll),
+           Poll = str_trim(gsub("Sunday Star.Times.", "", Poll))) %>%
+    rename(Pollster = Poll)
 
+table(polls$Client)
+table(polls$Pollster)
 test_that("Right number of pollsters", {
-    expect_equal(length(unique(polls$Poll)), 8)
+    expect_equal(length(unique(polls$Pollster)), 11)
 })
 
 test_that("Right number of parties", {
@@ -25,12 +41,12 @@ test_that("Dates Valid", {
     expect_equal(sum(is.na(polls$MidDate)), 0)    
 })
 
-# polls %>% filter(EndDate - StartDate > 32)
-
 test_that("Date ranges plausible", {
     expect_lt(with(polls, as.numeric(max(EndDate - StartDate))), 32)
     expect_equal(with(polls, as.numeric(min(EndDate - StartDate))), 0)
 })
+
+# polls %>% filter(EndDate - StartDate > 32 | EndDate < StartDate)
 
 save(polls, file = "pkg1/data/polls.rda")
 
@@ -41,13 +57,16 @@ library(forcats)
 
 polls %>%
     mutate(Party = fct_reorder(Party, VotingIntention, .desc = TRUE),
-           Poll = fct_relevel(Poll, "Election result")) %>%
-    ggplot(aes(x = MidDate, y = VotingIntention, colour = Party, shape = Poll, linetype = Poll)) +
+           Poll = fct_relevel(Pollster, "Election result")) %>%
+    ggplot(aes(x = MidDate, y = VotingIntention, linetype = Pollster)) +
     geom_line(alpha = 0.5) +
-    geom_point() +
-    geom_smooth(aes(group = Party), se = FALSE, colour = "grey15") +
-    scale_colour_manual(values = parties_v, guide = FALSE) +
+    geom_point(aes(colour = Client)) +
+    geom_smooth(aes(group = Party), se = FALSE, colour = "grey15", span = .20) +
+#    scale_colour_manual(values = parties_v, guide = FALSE) +
     scale_y_continuous("Voting intention", label = percent) +
     facet_wrap(~Party, scales = "free_y") +
     theme_grey() +
     theme(legend.position = c(0.8, 0.15))
+
+
+# see https://github.com/diegovalle/Elections-2012/blob/master/src/kalman.R to adapt for kalman filter
