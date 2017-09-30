33120e2d3e296cfbcd74a97de797dcfdf65bb03c
Peter Ellis <peter.ellis2013nz@gmail.com>
2017-10-01 12:35:30 +1300

    successfully import and tidy all the polling places back to 2002

--- prep/election_results/tidy_votingplace_results.R
+++ prep/election_results/tidy_votingplace_results.R
@@ -3,122 +3,143 @@
 # So far only for 2014 General Election
 # Peter Ellis, April 2016
 
-library(tidyr)
-library(dplyr)
-library(stringr)
+#===================define a function================
+tidy_results <- function(election_year = 2014, encoding = "UTF-8-BOM"){
+    
+    # where are the files and how many?
+    origin <- paste0("downloads/elect", election_year, "/")
+    number_electorates = length(list.files(origin)) / 2
+    
+    #---------------------candidate results-------------------
+    filenames <- paste0(origin, "cand_", 1:number_electorates, ".csv")
+    
+    results_voting_place <- list()
+    
+    for (i in 1:number_electorates){
+        
+        # What is the electorate name?  Is in cell A2 of each csv
+        electorate <- read.csv(filenames[i], skip=1, nrows=1, header=FALSE, 
+                               stringsAsFactors=FALSE, fileEncoding = encoding)[,1]
+        
+        # read in the bulk of the data
+        tmp <- tryCatch({
+            read.csv(filenames[i], skip=2, check.names=FALSE, stringsAsFactors=FALSE, fileEncoding = encoding)
+        }, warning = function(w) {
+            ## do something about the warning, maybe return 'NA'
+            # message(paste0("Got warnings with UTF-8-BOM encoding for file #", i, ", so trying without it"))
+            tmp <- read.csv(filenames[i], skip=2, check.names=FALSE, stringsAsFactors=FALSE)    
+        })
 
-#==================2014======================================
-number_electorates <- 71
-
-#---------------------2014 candidate results-------------------
-filenames <- paste0("downloads/elect2014/e9_part8_cand_", 1:number_electorates, ".csv")
-
-results_voting_place <- list()
-
-for (i in 1:number_electorates){
+      
+        # read in the candidate names and parties  
+        first_blank <- which(tmp[,2] == "")
+        candidates_parties <- tmp[(first_blank + 2) : nrow(tmp), 1:2]
+        names(candidates_parties) <- c("Candidate", "Party")
+        candidates_parties <- rbind(candidates_parties,
+                                    data.frame(Candidate="Informal Candidate Votes", Party="Informal Candidate Votes"))
+        
+        # we knock out all the rows from (first_blank - 1) (which is the total)
+        tmp <- tmp[-((first_blank - 1) : nrow(tmp)), ]
+        names(tmp)[1:2] <- c("ApproxLocation", "VotingPlace")
+        
+        # in some of the data there are annoying subheadings in the second column.  We can
+        # identify these as rows that return NA when you sum up where the votes should be
+        tmp <- tmp[!is.na(apply(tmp[, -(1:2)], 1, function(x){sum(as.numeric(x))})), ]
+        
+        # need to fill in the gaps where there is no polling location
+        last_ApproxLocation <- tmp[1, 1]
+        for(j in 1: nrow(tmp)){
+            if(tmp[j, 1] == "") {
+                tmp[j, 1] <- last_ApproxLocation
+            } else {
+                last_ApproxLocation <- tmp[j, 1]
+            }
+        }  
+        
+        # normalise / tidy
+        tmp <- tmp[names(tmp) != "Total Valid Candidate Votes"] %>%
+            gather(Candidate, Votes, -ApproxLocation, -VotingPlace)
+        
+        tmp$Electorate <- electorate
+        tmp <- tmp %>%
+            left_join(candidates_parties, by = "Candidate")
+        results_voting_place[[i]] <- tmp
+    }
     
-    # What is the electorate name?  Is in cell A2 of each csv
-    electorate <- read.csv(filenames[i], skip=1, nrows=1, header=FALSE, 
-                           stringsAsFactors=FALSE, fileEncoding = "UTF-8-BOM")[,1]
+    # combine all electorates
+    candidate_results_voting_place <- do.call("rbind", results_voting_place) %>%
+        mutate(Votes = as.numeric(Votes),
+               Party = gsub("M..ori Party", "Maori Party", Party),
+               VotingType = "Candidate")
     
-    # read in the bulk of the data
-    tmp <- read.csv(filenames[i], skip=2, check.names=FALSE, 
-                    stringsAsFactors=FALSE, fileEncoding = "UTF-8-BOM")
-  
-    # read in the candidate names and parties  
-    first_blank <- which(tmp[,2] == "")
-    candidates_parties <- tmp[(first_blank + 2) : nrow(tmp), 1:2]
-    names(candidates_parties) <- c("Candidate", "Party")
-    candidates_parties <- rbind(candidates_parties,
-                                data.frame(Candidate="Informal Candidate Votes", Party="Informal Candidate Votes"))
+    #-------------------party votes--------------------
+    filenames <- paste0(origin, "party_", 1:number_electorates, ".csv")
     
-    # we knock out all the rows from (first_blank - 1) (which is the total)
-    tmp <- tmp[-((first_blank - 1) : nrow(tmp)), ]
-    names(tmp)[1:2] <- c("ApproxLocation", "VotingPlace")
+    results_voting_place <- list()
     
-    # in some of the data there are annoying subheadings in the second column.  We can
-    # identify these as rows that return NA when you sum up where the votes should be
-    tmp <- tmp[!is.na(apply(tmp[, -(1:2)], 1, function(x){sum(as.numeric(x))})), ]
+    for (i in 1:number_electorates){
+        
+        # What is the electorate name?  Is in cell A2 of each csv
+        electorate <- read.csv(filenames[i], skip=1, nrows=1, header=FALSE, 
+                               stringsAsFactors=FALSE, fileEncoding = "UTF-8-BOM")[,1]
+        
+        # read in the bulk of the data
+        tmp <- tryCatch({
+            read.csv(filenames[i], skip=2, check.names=FALSE, stringsAsFactors=FALSE, fileEncoding = encoding)
+        }, warning = function(w) {
+            ## do something about the warning, maybe return 'NA'
+            # message(paste0("Got warnings with UTF-8-BOM encoding for file #", i, ", so trying without it"))
+            tmp <- read.csv(filenames[i], skip=2, check.names=FALSE, stringsAsFactors=FALSE)    
+        })
+        
+        first_blank <- which(tmp[, 2] == "")[1]
+        
+        # we knock out all the rows from (first_blank - 1) (which is the total)
+        tmp <- tmp[-((first_blank - 1) : nrow(tmp)), ]
+        names(tmp)[1:2] <- c("ApproxLocation", "VotingPlace")
+        
+        # in some of the data there are annoying subheadings in the second column.  We can
+        # identify these as rows that return NA when you sum up where the votes should be
+        tmp <- tmp[!is.na(apply(tmp[, -(1:2)], 1, function(x){sum(as.numeric(x))})), ]
+        
+        # need to fill in the gaps where there is no polling location
+        last_ApproxLocation <- tmp[1, 1]
+        for(j in 1: nrow(tmp)){
+            if(tmp[j, 1] == "") {
+                tmp[j, 1] <- last_ApproxLocation
+            } else {
+                last_ApproxLocation <- tmp[j, 1]
+            }
+        }  
+        
+        tmp <- tmp[names(tmp) != "Total Valid Party Votes"] %>%
+            gather(Party, Votes, -ApproxLocation, -VotingPlace)
+        
+        tmp$Electorate <- electorate
+        results_voting_place[[i]] <- tmp
+    }
     
-    # need to fill in the gaps where there is no polling location
-    last_ApproxLocation <- tmp[1, 1]
-    for(j in 1: nrow(tmp)){
-        if(tmp[j, 1] == "") {
-            tmp[j, 1] <- last_ApproxLocation
-        } else {
-            last_ApproxLocation <- tmp[j, 1]
-        }
-    }  
+    party_results_voting_place <- do.call("rbind", results_voting_place) %>%
+        mutate(Votes = as.numeric(Votes),
+               Party = gsub("M..ori Party", "Maori Party", Party),
+               VotingType = "Party")
     
-    # normalise / tidy
-    tmp <- tmp[names(tmp) != "Total Valid Candidate Votes"] %>%
-        gather(Candidate, Votes, -ApproxLocation, -VotingPlace)
+    #---------------combine the party and candidate voting place results--------------
+    combined <- party_results_voting_place %>%
+        mutate(Candidate = NA) %>%
+        rbind(candidate_results_voting_place)  %>%
+        mutate(voting_place = str_trim(VotingPlace),
+               approx_location = str_trim(ApproxLocation),
+               candidate = str_trim(Candidate),
+               election_year = election_year)
     
-    tmp$Electorate <- electorate
-    tmp <- tmp %>%
-        left_join(candidates_parties, by = "Candidate")
-    results_voting_place[[i]] <- tmp
+    return(combined)
+    
 }
 
-# combine all electorates
-candidate_results_voting_place <- do.call("rbind", results_voting_place) %>%
-    mutate(Votes = as.numeric(Votes),
-           Party = gsub("M..ori Party", "Maori Party", Party),
-           VotingType = "Candidate")
+#==============execute for the various years===============
 
-#-------------------2014 party votes--------------------
-filenames <- paste0("downloads/elect2014/e9_part8_party_", 1:number_electorates, ".csv")
+results <- lapply(c(2002, 2005, 2008, 2011, 2014), tidy_results)
+nzge <- do.call("rbind", results)
 
-results_voting_place <- list()
-
-for (i in 1:number_electorates){
-    
-    # What is the electorate name?  Is in cell A2 of each csv
-    electorate <- read.csv(filenames[i], skip=1, nrows=1, header=FALSE, 
-                           stringsAsFactors=FALSE, fileEncoding = "UTF-8-BOM")[,1]
-    
-    # read in the bulk of the data
-    tmp <- read.csv(filenames[i], skip=2, check.names=FALSE, 
-                    stringsAsFactors=FALSE, fileEncoding = "UTF-8-BOM")
-    
-    first_blank <- which(tmp[, 2] == "")[1]
-    
-    # we knock out all the rows from (first_blank - 1) (which is the total)
-    tmp <- tmp[-((first_blank - 1) : nrow(tmp)), ]
-    names(tmp)[1:2] <- c("ApproxLocation", "VotingPlace")
-    
-    # in some of the data there are annoying subheadings in the second column.  We can
-    # identify these as rows that return NA when you sum up where the votes should be
-    tmp <- tmp[!is.na(apply(tmp[, -(1:2)], 1, function(x){sum(as.numeric(x))})), ]
-    
-    # need to fill in the gaps where there is no polling location
-    last_ApproxLocation <- tmp[1, 1]
-    for(j in 1: nrow(tmp)){
-        if(tmp[j, 1] == "") {
-            tmp[j, 1] <- last_ApproxLocation
-        } else {
-            last_ApproxLocation <- tmp[j, 1]
-        }
-    }  
-    
-    tmp <- tmp[names(tmp) != "Total Valid Party Votes"] %>%
-        gather(Party, Votes, -ApproxLocation, -VotingPlace)
-    
-    tmp$Electorate <- electorate
-    results_voting_place[[i]] <- tmp
-}
-
-party_results_voting_place <- do.call("rbind", results_voting_place) %>%
-    mutate(Votes = as.numeric(Votes),
-           Party = gsub("M..ori Party", "Maori Party", Party),
-           VotingType = "Party")
-
-#---------------combine the 2014 voting place results--------------
-GE2014 <- party_results_voting_place %>%
-    mutate(Candidate = NA) %>%
-    rbind(candidate_results_voting_place)  %>%
-    mutate(VotingPlace = str_trim(VotingPlace),
-           ApproxLocation = str_trim(ApproxLocation),
-           Candidate = str_trim(Candidate))
-
-save(GE2014, file = "pkg1/data/GE2014.rda", compress = "xz")
+save("nzge", file = "pkg1/data/nzge.rda", compress = "xz")
