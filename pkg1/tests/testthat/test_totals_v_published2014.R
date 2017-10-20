17ad469f017b4521ef283fcbfba142b72cd94d0e
Peter Ellis <peter.ellis2013nz@gmail.com>
2017-10-21 11:42:29 +1300

    remove the old GE2014 and Locations2014 objects

--- pkg1/tests/testthat/test_totals_v_published2014.R
+++ pkg1/tests/testthat/test_totals_v_published2014.R
@@ -4,19 +4,21 @@ require(dplyr)
 # Must match results from 
 # http://www.electionresults.govt.nz/electionresults_2014/e9/html/e9_part1.html
 
+GE2014 <- filter(nzge, election_year == 2014)
+
 # absolute numbers
 expect_equal(
-    sum(filter(GE2014, Party == "National Party" & VotingType == "Party")$Votes),
+    sum(filter(GE2014, party == "National Party" & voting_type == "Party")$votes),
     1131501
 )
 
 expect_equal(
-    sum(filter(GE2014, Party == "National Party" & VotingType == "Candidate")$Votes),
+    sum(filter(GE2014, party == "National Party" & voting_type == "Candidate")$votes),
     1081787
 )
 
 expect_equal(
-    sum(filter(GE2014, Party == "Labour Party" & VotingType == "Party")$Votes),
+    sum(filter(GE2014, party == "Labour Party" & voting_type == "Party")$votes),
     604535
 )
 
@@ -26,17 +28,17 @@ expect_equal(
 )
 
 expect_equal(
-    sum(filter(GE2014, Party == "Labour Party" & VotingType == "Candidate")$Votes),
+    sum(filter(GE2014, party == "Labour Party" & voting_type == "Candidate")$votes),
     801287
 )
 
 expect_equal(
-    sum(filter(GE2014, Party == "New Zealand First Party" & VotingType == "Party")$Votes),
+    sum(filter(GE2014, party == "New Zealand First Party" & voting_type == "Party")$votes),
     208300
 )
 
 expect_equal(
-    sum(filter(GE2014, Party == "New Zealand First Party" & VotingType == "Candidate")$Votes),
+    sum(filter(GE2014, party == "New Zealand First Party" & voting_type == "Candidate")$votes),
     73384
 )
 
@@ -47,21 +49,21 @@ expect_equal(
 
 
 expect_equal(
-    sum(filter(GE2014, Party == "Patriotic Revolutionary Front" & VotingType == "Candidate")$Votes),
+    sum(filter(GE2014, party == "Patriotic Revolutionary Front" & voting_type == "Candidate")$votes),
     48
 )
 
 
 # percentages
 expect_equal(
-    round(sum(filter(GE2014, Party == "National Party" & VotingType == "Party")$Votes) /
-        sum(filter(GE2014, VotingType == "Party" & Party != "Informal Party Votes")$Votes) * 100, 2),
+    round(sum(filter(GE2014, party == "National Party" & voting_type == "Party")$votes) /
+        sum(filter(GE2014, voting_type == "Party" & party != "Informal Party Votes")$votes) * 100, 2),
     47.04
  )
 
 
 expect_equal(
-    round(sum(filter(GE2014, Party == "Green Party" & VotingType == "Candidate")$Votes) /
-              sum(filter(GE2014, VotingType == "Candidate" & Party != "Informal Candidate Votes")$Votes) * 100, 2),
+    round(sum(filter(GE2014, party == "Green Party" & voting_type == "Candidate")$votes) /
+              sum(filter(GE2014, voting_type == "Candidate" & party != "Informal Candidate Votes")$votes) * 100, 2),
     7.06
 )
