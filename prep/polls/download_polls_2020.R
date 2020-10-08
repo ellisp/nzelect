889f1debd69e33065b29daa18fc773479704c97e
Peter Ellis <peter.ellis2013nz@gmail.com>
2020-10-08 19:14:41 +1100

    one more CB poll

--- prep/polls/download_polls_2020.R
+++ prep/polls/download_polls_2020.R
@@ -18,7 +18,8 @@ names(tab)[1] <- "WikipediaDates"
 # head(tab[1])
 
 tab$StartDate <- 
-    as.Date(c("23/9/2020", 
+    as.Date(c("3/10/2020", 
+              "23/9/2020", 
               "16/9/2020",
               "17/9/2020",
               "1/8/2020",
@@ -43,7 +44,8 @@ tab$StartDate <-
               "2/10/2017", "23/9/2017"), 
             format = "%d/%m/%Y")
 
-tab$EndDate <- as.Date(c("27/9/2020", 
+tab$EndDate <- as.Date(c("7/10/2020", 
+                         "27/9/2020", 
                          "23/9/2020",
                          "21/9/2020",
                          "31/8/2020",
